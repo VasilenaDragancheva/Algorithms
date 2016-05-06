683da7db2bd41fa61e872a91a04a08459fd78572
Vasilena Dragancheva <vasilena86@hotmail.com>
2016-05-06 09:45:12 +0300

    Topological sorting added

--- GraphsAndGraphAlgorithms/DFS-Graph-Traversal.Tests/UnitTestsGraphComponents.cs
+++ GraphsAndGraphAlgorithms/DFS-Graph-Traversal.Tests/UnitTestsGraphComponents.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
 public class UnitTestsGraphComponents
 {
     [TestMethod]
-    public void TestGraphConnectedComponents9Vertices()
+    public void TestGraphConnectedComponents0Vertices()
     {
         // Arrange
-        var input =
-            "9" + "\n" +
-            "3 6" + "\n" +
-            "3 4 5 6" + "\n" +
-            "8" + "\n" +
-            "0 1 5" + "\n" +
-            "1 6" + "\n" +
-            "1 3" + "\n" +
-            "0 1 4" + "\n" +
-            "" + "\n" +
-            "2" + "\n";
+        var input = "0" + "\n";
 
         // Act
         var inputReader = new StringReader(input);
@@ -30,14 +21,11 @@ public class UnitTestsGraphComponents
             Console.SetOut(outputWriter);
             GraphConnectedComponents.Main();
         }
+
         var output = outputWriter.ToString();
 
         // Assert
-        var expectedOutput = 
-            "Connected component: 6 4 5 1 3 0" + "\n" +
-            "Connected component: 8 2" + "\n" +
-            "Connected component: 7" + "\n";
-        output = output.Replace("\r\n", "\n");
+        var expectedOutput = string.Empty;
         Assert.AreEqual(expectedOutput, output);
     }
 
@@ -45,9 +33,7 @@ public class UnitTestsGraphComponents
     public void TestGraphConnectedComponents1Vertex()
     {
         // Arrange
-        var input =
-            "1" + "\n" +
-            "0" + "\n";
+        var input = "1" + "\n" + "0" + "\n";
 
         // Act
         var inputReader = new StringReader(input);
@@ -58,70 +44,11 @@ public class UnitTestsGraphComponents
             Console.SetOut(outputWriter);
             GraphConnectedComponents.Main();
         }
+
         var output = outputWriter.ToString();
 
         // Assert
-        var expectedOutput =
-            "Connected component: 0\n";
-        output = output.Replace("\r\n", "\n");
-        Assert.AreEqual(expectedOutput, output);
-    }
-
-    [TestMethod]
-    public void TestGraphConnectedComponents0Vertices()
-    {
-        // Arrange
-        var input =
-            "0" + "\n";
-
-        // Act
-        var inputReader = new StringReader(input);
-        var outputWriter = new StringWriter();
-        using (outputWriter)
-        {
-            Console.SetIn(inputReader);
-            Console.SetOut(outputWriter);
-            GraphConnectedComponents.Main();
-        }
-        var output = outputWriter.ToString();
-
-        // Assert
-        var expectedOutput = "";
-        Assert.AreEqual(expectedOutput, output);
-    }
-
-    [TestMethod]
-    public void TestGraphConnectedComponents7Vertices()
-    {
-        // Arrange
-        var input =
-            "7" + "\n" +
-            "" + "\n" +
-            "2 6" + "\n" +
-            "1" + "\n" +
-            "4" + "\n" +
-            "3" + "\n" +
-            "" + "\n" +
-            "1" + "\n" +
-            "" + "\n";
-
-        // Act
-        var inputReader = new StringReader(input);
-        var outputWriter = new StringWriter();
-        using (outputWriter)
-        {
-            Console.SetIn(inputReader);
-            Console.SetOut(outputWriter);
-            GraphConnectedComponents.Main();
-        }
-        var output = outputWriter.ToString();
-
-        // Assert
-        var expectedOutput =
-            "Connected component: 0" + "\n" +
-            "Connected component: 2 6 1" + "\n" +
-            "Connected component: 4 3" + "\n" +
-            "Connected component: 5" + "\n";
+        var expectedOutput = "Connected component: 0\n";
         output = output.Replace("\r\n", "\n");
         Assert.AreEqual(expectedOutput, output);
     }
@@ -130,12 +57,7 @@ public class UnitTestsGraphComponents
     public void TestGraphConnectedComponents4Vertices()
     {
         // Arrange
-        var input =
-            "4" + "\n" +
-            "1 2 3" + "\n" +
-            "0 1 2 3" + "\n" +
-            "0 1 3" + "\n" +
-            "1 2" + "\n";
+        var input = "4" + "\n" + "1 2 3" + "\n" + "0 1 2 3" + "\n" + "0 1 3" + "\n" + "1 2" + "\n";
 
         // Act
         var inputReader = new StringReader(input);
@@ -146,11 +68,63 @@ public class UnitTestsGraphComponents
             Console.SetOut(outputWriter);
             GraphConnectedComponents.Main();
         }
+
         var output = outputWriter.ToString();
 
         // Assert
-        var expectedOutput =
-            "Connected component: 3 2 1 0\n";
+        var expectedOutput = "Connected component: 3 2 1 0\n";
+        output = output.Replace("\r\n", "\n");
+        Assert.AreEqual(expectedOutput, output);
+    }
+
+    [TestMethod]
+    public void TestGraphConnectedComponents7Vertices()
+    {
+        // Arrange
+        var input = "7" + "\n" + string.Empty + "\n" + "2 6" + "\n" + "1" + "\n" + "4" + "\n" + "3" + "\n" + string.Empty + "\n" + "1"
+                    + "\n" + string.Empty + "\n";
+
+        // Act
+        var inputReader = new StringReader(input);
+        var outputWriter = new StringWriter();
+        using (outputWriter)
+        {
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            GraphConnectedComponents.Main();
+        }
+
+        var output = outputWriter.ToString();
+
+        // Assert
+        var expectedOutput = "Connected component: 0" + "\n" + "Connected component: 2 6 1" + "\n"
+                             + "Connected component: 4 3" + "\n" + "Connected component: 5" + "\n";
+        output = output.Replace("\r\n", "\n");
+        Assert.AreEqual(expectedOutput, output);
+    }
+
+    [TestMethod]
+    public void TestGraphConnectedComponents9Vertices()
+    {
+        // Arrange
+        var input = "9" + "\n" + "3 6" + "\n" + "3 4 5 6" + "\n" + "8" + "\n" + "0 1 5" + "\n" + "1 6" + "\n" + "1 3"
+                    + "\n" + "0 1 4" + "\n" + string.Empty + "\n" + "2" + "\n";
+
+        // Act
+        var inputReader = new StringReader(input);
+        var outputWriter = new StringWriter();
+        using (outputWriter)
+        {
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            GraphConnectedComponents.Main();
+        }
+
+        var output = outputWriter.ToString();
+
+        // Assert
+        var expectedOutput = "Connected component: 6 4 5 1 3 0" + "\n" + "Connected component: 8 2" + "\n"
+                             + "Connected component: 7" + "\n";
         output = output.Replace("\r\n", "\n");
         Assert.AreEqual(expectedOutput, output);
     }
--- GraphsAndGraphAlgorithms/Topological-Sorting/TopologicalSorter.cs
+++ GraphsAndGraphAlgorithms/Topological-Sorting/TopologicalSorter.cs
@@ -1,18 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public class TopologicalSorter
 {
-    private Dictionary<string, List<string>> graph;
+    private readonly Dictionary<string, List<string>> graph;
+
+    private readonly Dictionary<string, int> predeccCount;
 
     public TopologicalSorter(Dictionary<string, List<string>> graph)
     {
         this.graph = graph;
+        this.predeccCount = new Dictionary<string, int>();
+        this.CountPredeccessors();
     }
 
     public ICollection<string> TopSort()
     {
-        // TODO: Implement the topological sorting algorithm
-        throw new NotImplementedException();
+        var removedNodes = new List<string>();
+        var vertextes = this.predeccCount.Keys.ToList();
+
+        while (removedNodes.Count < this.graph.Count)
+        {
+            foreach (var vertex in vertextes)
+            {
+                if (this.predeccCount[vertex] == 0 && !removedNodes.Contains(vertex))
+                {
+                    removedNodes.Add(vertex);
+
+                    if (this.graph.ContainsKey(vertex))
+                    {
+                        foreach (var vert in this.graph[vertex])
+                        {
+                            this.predeccCount[vert]--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return removedNodes;
+    }
+
+    private void CountPredeccessors()
+    {
+        foreach (var vertex in this.graph)
+        {
+            if (!this.predeccCount.ContainsKey(vertex.Key))
+            {
+                this.predeccCount[vertex.Key] = 0;
+            }
+
+            foreach (var vert in vertex.Value)
+            {
+                if (!this.predeccCount.ContainsKey(vert))
+                {
+                    this.predeccCount[vert] = 0;
+                }
+
+                this.predeccCount[vert]++;
+            }
+        }
     }
 }
